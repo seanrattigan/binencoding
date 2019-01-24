f3f82e3b35dc7bd65bf8fb77234e15bfeef57ec4
seanrattigan <sean.rattigan@gmail.com>
2019-01-24 18:29:22 +0000

    first draft for encoding strings to binary and decoding back again

---
+++ venv/Lib/bincode.py
@@ -0,0 +1,45 @@
+# ==================================================================
+# title           :bincode 
+# description     :
+# author          :srattigan
+# date            :24/01/2019
+# version         :1.0
+# notes           :draft
+# python_version  :3.7  
+# file_version    :1.00
+# ==================================================================
+
+# imports
+import menugen
+
+# globals and constants
+
+
+# functions
+def encode(plaintext):
+    """
+    (str)->str
+    takes plain text and converts to a binary code format
+    >>> encode("b")
+    '1000010'
+    >>> encode("bad")
+    '1000010/1000001/1100100'
+    """
+    pass
+
+
+def decode(cyphertext):
+    """
+    (str)->str
+    takes binary nums and converts to chars
+    >>> decode('1000010')
+    'b'
+    >>> encode('1000010/1000001/1100100')
+    'bad
+    """
+    pass
+
+# main body
+
+if __name__ == "__main__":
+    pass
