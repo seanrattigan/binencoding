d267b142c8ddd46ede3faebf850130e7db8b6d97
seanrattigan <sean.rattigan@gmail.com>
2019-01-29 20:00:12 +0000

    created main application for main menu for entire program

---
+++ venv/Lib/main_app.py
@@ -0,0 +1,25 @@
+# ==================================================================
+# title           :main_app 
+# description     :
+# author          :srattigan
+# date            :29/01/2019
+# version         :1.0
+# notes           :change from last version
+# python_version  :3.7  
+# file_version    :1.00
+# ==================================================================
+
+# imports
+import duckode_app as duk
+import bincode_app as bin
+
+# globals and constants
+
+
+# functions
+
+
+# main body
+
+if __name__ == "__main__":
+    pass
