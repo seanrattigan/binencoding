ac040c1a753dfe350b295429d1e715c4afefe886
seanrattigan <sean.rattigan@gmail.com>
2019-01-29 19:07:17 +0000

    created shell for duck encoding

---
+++ venv/Lib/duckode.py
@@ -0,0 +1,37 @@
+# ==================================================================
+# title           :duckode 
+# description     :
+# author          :srattigan
+# date            :29/01/2019
+# version         :1.0
+# notes           :change from last version
+# python_version  :3.7  
+# file_version    :1.00
+# ==================================================================
+
+# imports
+
+
+# globals and constants
+
+
+# functions
+def encode(plaintext, key):
+    """
+    (str, str)->str
+    Encodes plaintext as duck text
+    """
+    return "Duck encoding called successfully"
+
+
+def decode(plaintext, key):
+    """
+    (str, str)->str
+    Decodes cyphertext from duck text to plaintext
+    """
+    return "Duck decoding called successfully"
+
+# main body
+
+if __name__ == "__main__":
+    pass
