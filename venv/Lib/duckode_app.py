8272a68b464e98ed36c814cc2f99205656466e6e
seanrattigan <sean.rattigan@gmail.com>
2019-01-29 19:19:54 +0000

    tidied app

--- venv/Lib/duckode_app.py
+++ venv/Lib/duckode_app.py
@@ -17,7 +17,7 @@ import duckode
 
 
 # functions
-def get_string(opt="encode"):
+def get_string(opt=""):
     """
     (None)->str
     Gets a string from the user and returns it
@@ -34,11 +34,11 @@ if __name__ == "__main__":
     while True:
         response = bin_menu.display()
         if response == 0:
-            print("Encode to duck called")
+            print("Encode to duck called")  # may be commented out afterwards?
             cyph = duckode.encode(get_string("to encode text to duck"), get_string("for key"),  )
             print(cyph)
         elif response == 1:
-            print("Decode called")  # 1100011/1100001/1110100
+            print("Decode called")  # may be commented out afterwards?
             plain = duckode.decode(get_string("to decode duck ro text"), get_string("for key"), )
             print(plain)
         elif response == 2:
